e885fcc299f4dfe8aaffe168eeb7d2e6f219fe95
Bimlesh <bimlesh759kumar.ai@gmail.com>
2026-02-22 11:13:58 +0530

    feat: initial addition function

---
+++ app.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    a = float(input("Enter the first number: "))
+    b = float(input("Enter the second number: "))
+    result = add(a, b)
+    print(f"The result of adding {a} and {b} is: {result}")
